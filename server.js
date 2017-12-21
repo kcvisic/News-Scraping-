d78d753dd3019af266c0cac22c125f0cca31e87f
kcvisic <kcvisic@gmail.com>
2017-12-20 23:07:08 -0500

    add

--- server.js
+++ server.js
@@ -17,11 +17,18 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/News_Scraper", {
-  useMongoClient: true
-});
+
+// Connect to localhost if not a production environment
+if(process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else{
+  mongoose.connect("mongodb://localhost/News_Scraper", {
+  useMongoClient: true;
+  });
+}
+
 var db = mongoose.connection;
 
 // Show any Mongoose errors
